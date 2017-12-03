348fd7fd47bf998c7c1a2dfde55a4933d9f6e85b
eignnx <34199632+eignnx@users.noreply.github.com>
2017-12-02 21:19:27 -0500

    Read args (#1)
    * Add lapp as dependency, add gitignore
    
    * Copy-paste example lapp arg parser into main.rs
    
    * Customize lapp arg parser for project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate lapp;
+
 fn main() {
-    println!("Hello, world!");
+    let args = lapp::parse_args("
+    Prints out first n lines of a file
+        <makefile> (default 'make.toml') name of makefile
+        <mode> (default 'dev') compilation mode
+    ");
+
+    let makefile_name = args.get_string("makefile");
+    let compilation_mode = args.get_string("mode");
+
+    println!("makefile: '{}', compilation mode: '{}'",
+        makefile_name, compilation_mode);
 }
